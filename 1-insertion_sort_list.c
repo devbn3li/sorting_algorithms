951010ee4f60f42a168d2cffb4f17d92c25bec70
devbn3li <fudwhc@gmail.com>
2023-09-26 05:14:49 +0300

    up

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,34 +10,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *crnt, *temp;
+	listint_t *crnt, *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-    return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	return;
 
-    crnt = (*list)->next;
+	crnt = (*list)->next;
 
-    while (crnt != NULL)
-    {
-        temp = crnt;
+	while (crnt != NULL)
+	{
+		temp = crnt;
 
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            temp->prev->next = temp->next;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
 
-            if (temp->next != NULL)
-            temp->next->prev = temp->prev;
+			if (temp->next != NULL)
+			temp->next->prev = temp->prev;
 
-            temp->next = temp->prev;
-            temp->prev = temp->prev->prev;
-            temp->next->prev = temp;
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
 
-            if (temp->prev == NULL)
-            *list = temp;
+			if (temp->prev == NULL)
+			*list = temp;
 
-            print_list(*list);
-        }
+			print_list(*list);
+		}
 
-        crnt = crnt->next;
-    }
+		crnt = crnt->next;
+	}
 }
